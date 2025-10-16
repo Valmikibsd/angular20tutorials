365873508adceee0b4025011d843263a588fcc94
Valmiki yadav <Valmiki@bsdinfotech.com>
2025-10-16 13:04:23 +0530

    post delete api

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Controlflow } from './compnents/controlflow/controlflow';
 import { SignalEx } from './compnents/signal-ex/signal-ex';
 import { AttDirective } from './compnents/att-directive/att-directive';
 import { Getapi } from './compnents/getapi/getapi';
+import { User } from './compnents/user/user';
 
 export const routes: Routes = [
     {
@@ -25,5 +26,10 @@ export const routes: Routes = [
     {
         path:"GetApi",
         component: Getapi
+    }
+    ,
+    {
+        path:"User",
+        component: User
     }
 ];
--- src/app/compnents/user/user.ts
+++ src/app/compnents/user/user.ts
@@ -1,11 +1,93 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user.html',
   styleUrl: './user.css'
 })
-export class User {
+export class User  {
 
+  http=inject(HttpClient);
+  userlist:any[]=[];
+  
+  useobj={
+      "userId": 0,
+      "emailId": "",
+      "password": "",
+      "fullName": "",
+      "mobileNo": ""
+  }
+
+  ngOnInit(): void {
+    this.getusers();
+  }
+
+  register(){
+
+    this.http.post("https://api.freeprojectapi.com/api/GoalTracker/register",this.useobj)
+    .subscribe({next:(res:any)=>{
+      console.log(res);
+      alert("Registered Successfully");
+      this.getusers();
+      this.clear();
+    },error:(err:any)=>{
+      console.log(err);
+      alert("Something went wrong");
+    }
+
+    });
+  }
+
+  update(){
+    this.http.put("https://api.freeprojectapi.com/api/GoalTracker/register",this.useobj)
+    .subscribe({next:(res:any)=>{
+      console.log(res);
+      alert("Updated Successfully");
+      this.getusers();
+
+    },
+    error:(err:any)=>{
+
+      console.log(err);
+      alert("Something went wrong");
+    }});
+  }
+
+  getusers(){
+    this.http.get("https://api.freeprojectapi.com/api/GoalTracker/getAllUsers")
+    .subscribe((res:any)=>{
+      console.log(res);
+      this.userlist=res;
+      console.log(this.userlist);
+    });
+
+  }
+
+  deleteuser(userId:number){
+    this.http.delete("https://api.freeprojectapi.com/api/GoalTracker/deleteUserById?id="+userId)
+    .subscribe({next:(res:any)=>{
+      console.log(res);
+      alert("Deleted Successfully");
+      this.getusers();
+    },error:(err:any)=>{
+      console.log(err);
+      alert("Something went wrong");
+    }});
+  }
+  edituser(user:any){
+    this.useobj=user;
+  }
+
+  clear(){
+    this.useobj={
+      "userId": 0,
+      "emailId": "",
+      "password": "",
+      "fullName": "",
+      "mobileNo": ""
+  }
+  }
 }
